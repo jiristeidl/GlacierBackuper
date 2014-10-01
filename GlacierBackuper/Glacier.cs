d1350f6a4e6be97a09d63c75d8fccf4746e0cca6
jiristeidl <jiristeidl+github@gmail.com>
2014-10-01 21:31:27 +0200

    Writing Vault info

---
+++ GlacierBackuper/Glacier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Amazon.Glacier;
+using Amazon.Glacier.Transfer;
+using Amazon.Glacier.Model;
+
+namespace GlacierBackuper
+{
+    internal class Glacier
+    {
+        private AmazonGlacierClient client { get; set; }
+
+        public DescribeVaultResponse GetVaultDescriptions(string vaultName)
+        {
+            DescribeVaultRequest request = new DescribeVaultRequest(vaultName);
+
+            return client.DescribeVault(request);
+        }
+
+        public Glacier()
+        {
+            client = new AmazonGlacierClient(new AmazonGlacierConfig());
+        }
+
+        public List<DescribeVaultOutput> GetVaultsList()
+        {
+            return client.ListVaults().VaultList;
+        }
+    }
+}
